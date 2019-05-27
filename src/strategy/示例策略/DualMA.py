695288c20239b5f9e714e376aa790009fd96f29d
lmx-hub <lmxeq5@qq.com>
2019-05-27 16:57:09 +0800

    Update DualMA.py

--- src/strategy/示例策略/DualMA.py
+++ src/strategy/示例策略/DualMA.py
@@ -1,15 +1,10 @@
 #-*-coding:utf-8
 import talib
 
-buyPos = 0
-sellPos = 0
-
 def initialize(context):
     SetBarInterval("SHFE|F|CU|1907", 'M',1, 2000)
 
-def handle_data(context):
-    global buyPos, sellPos
-    
+def handle_data(context):    
     ma1 = talib.MA(Close(), timeperiod=5)
     ma2 = talib.MA(Close(), timeperiod=20)
     
@@ -21,8 +16,6 @@ def handle_data(context):
         
     if MarketPosition() != 1 and ma1[-1] > ma2[-1]:
         Buy(1, Open()[-1])               # 买开仓
-        buyPos += 1
         
     if MarketPosition() != -1 and ma1[-1] < ma2[-1]:
         SellShort(1, Open()[-1])         # 卖开仓
-        sellPos += 1
