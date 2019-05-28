1c7e1eaa8f27278f1e1f409b9ca2968c9dede8de
gaoyantao134627 <603172532@qq.com>
2019-05-28 19:36:18 +0800

    多合约示例

---
+++ src/strategy/示例策略/SpdTrade1.py
@@ -0,0 +1,82 @@
+# 套利策略 历史回测
+
+import talib
+
+code1="ZCE|F|TA|909"
+code2="ZCE|F|TA|001"
+p1=20
+dot=2
+qty=1
+
+bar=0
+
+def initialize(context):
+    SetBarInterval(code1, 'M', 1, 1000)
+    SetBarInterval(code2, 'M', 1, 2000)
+    
+def handle_data(context):
+    if len(Close(code1, 'M', 1)) < p1 or len(Close(code2, 'M', 1)) < p1:
+        return;
+
+    ma1 = talib.MA(Close(code1, 'M', 1), timeperiod=p1, matype=0)
+    ma2 = talib.MA(Close(code2, 'M', 1), timeperiod=p1, matype=0)    
+    LogInfo('ma1', Close(code1, 'M', 1)[-1], '  ma2', Close(code2, 'M', 1)[-1])
+    
+    print("ma1:", ma1[-1], "ma2:",ma2[-1]) 
+    ma= ma1[-1] - ma2[-1]
+    prc = Close(code1, 'M', 1)[-1] - Close(code2, 'M', 1)[-1]       
+    
+    PlotNumeric("ma1", ma1[-1], color=0xFF00FF)
+    PlotNumeric("ma2", ma2[-1], color=0x0008FF)
+    
+    PlotNumeric("prc", prc, 0xFF0000, False)
+    PlotNumeric("high", ma + dot * PriceTick(), 0x00aa00, False)
+    PlotNumeric("low", ma - dot * PriceTick(), 0x0000FF, False)    
+    PlotNumeric("fit", NetProfit(), 0xFF0000, False)
+    
+    global bar    
+    #同一根K线上只做一笔交易
+    if bar == CurrentBar():
+        return
+
+    if prc > ma + dot * PriceTick():
+        if MarketPosition() == 0:
+            Buy(qty, Close()[-1])
+            Buy(qty, Close()[-1])
+        elif MarketPosition() < 0:
+            BuyToCover(qty, Close()[-1])
+            BuyToCover(qty, Close()[-1])
+        else:
+            return
+    elif prc < ma - dot * PriceTick():
+        if MarketPosition() == 0:
+            SellShort(qty, Close()[-1])
+            SellShort(qty, Close()[-1])
+        elif MarketPosition() > 0:
+            Sell(qty, Close()[-1])
+            Sell(qty, Close()[-1])
+        else:
+            return
+        
+    bar = CurrentBar()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
