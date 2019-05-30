8cc2ddb429cbc631e403b1080c5b8832396784e0
lmx-hub <lmxeq5@qq.com>
2019-05-30 10:49:27 +0800

    修改策略

--- src/strategy/示例策略/SpdTrade2.py
+++ src/strategy/示例策略/SpdTrade2.py
@@ -4,37 +4,38 @@ import talib
 
 usr="DDY"
 code1="ZCE|F|TA|909"
-code2="ZCE|F|AT|001"
+code2="ZCE|F|TA|001"
+tp = 'M'
+p = 10
+
+p1=5
 p1=20
-dot=2
+dot=1
 qty=1
 
 bar=0
 
 def initialize(context):
-    SetBarInterval(code1, 'M', 10, 'N')
-    SetBarInterval(code2, 'M', 10, 'N')
+    SetBarInterval(code1, 'M', p, 1)
+    SetBarInterval(code2, 'M', p, 1)
     SetTriggerType(code1, 1)
     SetTriggerType(code2, 1)
     SetActual()
 
 def handle_data(context):
-    if len(Close()) < p1:
+    if len(Close(code1, tp, p)) < p1 or len(Close(code2, tp, p)) < p2:
         return;
-    ma1 = talib.MA(Close(code1), timeperiod=p1, matype=0)
-    ma2 = talib.MA(Close(code2), timeperiod=p1, matype=0)
-    LogInfo('ma1', Close(code1)[-1], '  ma2', Close(code2)[-1])
 
-    ma= ma1[-1] - ma2[-1]
-    prc = Close(code1)[-1] - Close(code2)[-1]       
+    ma1 = talib.MA(Close(code1, tp, p), timeperiod=p1, matype=0)
+    ma2 = talib.MA(Close(code2, tp, p), timeperiod=p1, matype=0)
+    sma1= ma1[-1] - ma2[-1]
+    ma1 = talib.MA(Close(code1, tp, p), timeperiod=p2, matype=0)
+    ma2 = talib.MA(Close(code2, tp, p), timeperiod=p2, matype=0)
+    sma2= ma1[-1] - ma2[-1]     
     
-    PlotNumeric("ma1", ma1[-1], color=0xFF00FF)
-    PlotNumeric("ma2", ma2[-1], color=0x0008FF)
-    
-    PlotNumeric("prc", prc, 0xFF0000, False)
-    PlotNumeric("high", ma + dot * PriceTick(), 0x00aa00, False)
-    PlotNumeric("low", ma - dot * PriceTick(), 0x0000FF, False)
-    PlotNumeric("fit", A_TotalProfitLoss(), 0xFF0000, False) 
+    PlotNumeric("sma1", sma1, 0x0000FF, False)
+    PlotNumeric("sma2", sma2, 0xFF0000, False)
+    PlotNumeric("profit", A_TotalProfitLoss(), 0x808080, False, True) 
     
     if context.strategyStatus() !='C':
         return;
@@ -45,7 +46,7 @@ def handle_data(context):
         return
 
     offset = Enum_Entry()
-    if prc > ma + dot * PriceTick():
+    if sma1 > sma2 + dot * PriceTick():
         # 不允许多笔多仓
         if A_TotalPosition() > 0:
             return
@@ -53,7 +54,7 @@ def handle_data(context):
             offset = Enum_ExitToday()
         A_SendOrder(usr, code1, '2', '0', Enum_Buy() , offset, 'T', Q_BidPrice(code1) + PriceTick(), qty)
         A_SendOrder(usr, code2, '2', '0', Enum_Sell(), offset, 'T', Q_AskPrice(code2) - PriceTick(), qty)
-    elif prc < ma - dot * PriceTick():
+    elif sma1 < sma2 - dot * PriceTick():
         # 不允许多笔空仓
         if A_TotalPosition() < 0:
             return
