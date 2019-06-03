8f380e98b4877f0632dd9a1b672f3e2c2a421fb8
gaoyantao134627 <603172532@qq.com>
2019-06-03 08:25:34 +0800

    多合约示例策略

--- src/strategy/示例策略/DualMA.py
+++ src/strategy/示例策略/DualMA.py
@@ -4,7 +4,7 @@ p1=5
 p2=20
 
 def initialize(context): 
-    SetBarInterval("ZCE|F|TA|909", 'M', 1, 1000)
+    SetBarInterval("NYMEX|F|CL|1909", 'M', 1, 10)
     SetActual()
 
 def handle_data(context):
