fdb6cd607653f10b48400c5dd35230b8f786a284
wangpj12 <1582718607@qq.com>
2019-06-03 09:24:12 +0800

    修改运行设置合约解析错误

--- src/ui/com_view.py
+++ src/ui/com_view.py
@@ -1442,8 +1442,8 @@ class RunWin(QuantToplevel, QuantFrame):
                 self.toSampFrame()
                 return
 
-        #self.config["Contract"] = (contractInfo,)
-        self.config["Contract"] = tuple(contractInfo)
+        self.config["Contract"] = (contractInfo,)
+        # self.config["Contract"] = tuple(contractInfo)
         self.config["Trigger"]["Cycle"] = int(cycle) if isCycle else None
         self.config["Trigger"]["Timer"] = timerFormatter if timer else None
         self.config["Trigger"]["KLine"] = True if isKLine else False
