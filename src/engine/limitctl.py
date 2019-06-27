3b03135aca5881015f2091adcda03079a12adf2c
wangpj12 <1582718607@qq.com>
2019-06-27 16:02:17 +0800

    修改发单限制bug

--- src/engine/limitctl.py
+++ src/engine/limitctl.py
@@ -4,29 +4,51 @@ from capi.com_types import *
 
 class LimitCtl(object):
 
-    def __init__(self, maxConOpenTimes, maxKLineOpenTime, openNotReverse, coverNotOpen):
+    def __init__(self, maxConOpenTimes, maxCurBarOpenTime, openNotReverse, coverNotOpen):
 
         # 限制
         self._maxConOpenTime   = maxConOpenTimes      # 最大连续同向开仓次数
-        self._maxKLineOpenTime = maxKLineOpenTime     # 最大每根K线同向开仓次数
+        self._maxCurBarOpenTime = maxCurBarOpenTime   # 最大每根K线同向开仓次数
         self._openNotReverse   = openNotReverse       # 开仓的当前K线不允许平仓
         self._coverNotOpen     = coverNotOpen         # 平仓的当前K线不允许开仓
 
-        # self._conOpen          = defaultdict(int)   # 连续同向开仓次数(多合约字典)
-        self._conOpen          = 0                    # 连续同向开仓次数
+        self._conOpenTime          = 0                # 连续同向开仓次数
 
-        self._kLineOpenTime    = 0                    # 每根K线同向开仓次数
+        self._curBarOpenTime    = self._initCurBarOpenTime()   # 每根K线同向开仓次数
 
         self._hasCover         = False                # 当前K线是否有平仓单
         self._hasOpen          = False                # 当前K线是否有开仓单
 
         self._curBarIndex      = 0                    # 当前bar序号
 
-
     def _setCurBarIndex(self, index):
         if index == self._curBarIndex:
             return
         self._curBarIndex = index
+
+    def _initCurBarOpenTime(self):
+        self._curBarOpenTime = {
+            "BuyOpen" : 0,
+            "SellOpen":  0,
+            "Buy"     :  0,
+            "Sell"    :  0
+        }
+
+    def _setDCurBarOpenTime(self, order):
+        """设置某一方向的开仓次数"""
+        if order["Direct"] == dBuy and order["Offset"] == oOpen:  # 买开
+            self._curBarOpenTime["BuyOpen"] += 1
+
+        elif order["Direct"] == dSell and order["Offset"] == oOpen:  # 卖开
+            self._curBarOpenTime["SellOpen"] += 1
+
+        elif order["Direct"] == dBuy and order["Offset"] == oNone:  # 买
+            self._curBarOpenTime["Buy"] += 1
+
+        elif order["Direct"] == dSell and order["Offset"] == oNone:  # 卖
+            self._curBarOpenTime["Sell"] += 1
+        else:
+            pass
 
     def _allowConOpen(self, order, lastOrder):
         """
@@ -40,29 +62,17 @@ class LimitCtl(object):
             return 1  #  连续开仓次数不做限制
 
         if len(lastOrder) < 1:
-            if order["Offset"] == oOpen or order["Offset"] == oNone:
-                self._conOpen += 1
-
             return 1
 
         if order["Offset"] == oOpen or order["Offset"] == oNone:   # 开仓单或外盘订单
             if lastOrder["Direct"] == order["Direct"] and \
                     lastOrder["Offset"] == order["Offset"]:  # 订单方向相同
-                if self._conOpen >= self._maxConOpenTime:
-                    # self._conOpen[order["Cont"]] = 1
+                if self._conOpenTime >= self._maxConOpenTime:
                     return -1   # 大于同向开仓次数限制
-                else:
-                    self._conOpen += 1
-                    return 1  # 不大于同向开仓次数限制
-            else:
-                self._conOpen = 0
-                return 1  # 不同向
 
-        else:
-            self._conOpen = 0
-            return 1  # 小于同向开仓次数限制
+        return 1
 
-    def _allowCurBarOpen(self, order, lastOrder):
+    def _allowCurBarOpen(self, order):
         """
         每根K线同向开仓次数是否满足条件
         :param order: 订单
@@ -70,36 +80,30 @@ class LimitCtl(object):
         :return: 1：订单满足限制条件   -1： 订单不满足限制条件
         """
         # TODO：暂时没有考虑外盘
-        if self._maxKLineOpenTime == -1:
+        if self._maxCurBarOpenTime == -1:
             return 1   # 每根K线同向开仓次数不做限制
 
-        if len(lastOrder) < 1:
-            if order["Offset"] == oOpen or order["Offset"] == oNone:
-                self._kLineOpenTime += 1
-            return 1
-
         if order["CurBarIndex"] == self._curBarIndex:
-            if order["Offset"] == oOpen or order["Offset"] == oNone:  # 开仓单
-                if lastOrder["Direct"] == order["Direct"] and \
-                        lastOrder["Offset"] == order["Offset"]:  # 订单方向相同
-                    if self._kLineOpenTime >= self._maxKLineOpenTime:
+            if order["Offset"] == oOpen or order["Offset"] == oNone:
+                if order["Direct"] == dBuy and order["Offset"] == oOpen:  # 买开
+                    if self._curBarOpenTime["BuyOpen"] >= self._maxCurBarOpenTime:
                         return -1
-                    else:
-                        self._kLineOpenTime += 1
-                        return 1
-                else:   # 开仓不同向
-                    self._kLineOpenTime = 1
-                    return 1
-            else:      # 不是开仓单
-                return 1
 
-        else:
-            if order["Direct"] == oOpen or order["Direct"] == oNone:
-                self._kLineOpenTime = 1
-                return 1
-            else:
-                self._kLineOpenTime = 0
-                return 1
+                elif order["Direct"] == dSell and order["Offset"] == oOpen:  # 卖开
+                    if self._curBarOpenTime["SellOpen"] >= self._maxCurBarOpenTime:
+                        return -1
+
+                elif order["Direct"] == dBuy and order["Offset"] == oNone:  # 买
+                    if self._curBarOpenTime["Buy"] >= self._maxCurBarOpenTime:
+                        return -1
+
+                elif order["Direct"] == dSell and order["Offset"] == oNone:  # 卖
+                    if self._curBarOpenTime["Sell"] >= self._maxCurBarOpenTime:
+                        return -1
+                else:
+                    pass
+
+        return 1
 
     def _allowCover(self, order):
         """
@@ -115,22 +119,8 @@ class LimitCtl(object):
             if self._hasOpen:
                 if order["Offset"] == oCover:
                     return -1
-                else:
-                    return 1
-            else:
-                if order["Offset"] == oOpen:
-                    self._hasOpen = True
-                    return 1
-                else:
-                    return 1
 
-        else:
-            if order["Offset"] == oOpen:
-                self._hasOpen = True
-                return 1
-            else:
-                self._hasOpen = False
-                return 1
+        return 1
 
     def _allowOpen(self, order):
         """
@@ -146,22 +136,8 @@ class LimitCtl(object):
             if self._hasCover:     # 当前K线存在平仓单
                 if order["Offset"] == oOpen:
                     return -1
-                else:
-                    return 1
-            else:                                # 当前K线不存在平仓单
-                if order["Offset"] == oCover:
-                    self._hasCover = True
-                    return 1
-                else:
-                    return 1
 
-        else:
-            if order["Offset"] == oCover:
-                self._hasCover = True
-                return 1
-            else:
-                self._hasCover = False
-                return 1
+        return 1
 
     def allowOrder(self, order, lastOrder):
         """
@@ -172,16 +148,103 @@ class LimitCtl(object):
         """
 
         ret1 = self._allowConOpen(order, lastOrder)
-        ret2 = self._allowCurBarOpen(order, lastOrder)
+        ret2 = self._allowCurBarOpen(order)
         ret3 = self._allowCover(order)
         ret4 = self._allowOpen(order)
 
-        self._setCurBarIndex(order["CurBarIndex"])
-
         if ret1 == 1 and ret2 == 1 and ret3 == 1 and ret4 == 1:
+            self._setConOpenTime(order, lastOrder)
+            self._setCurBarOpenTime(order)
+            self._setHasOpen(order)
+            self._setHasCover(order)
+            self._setCurBarIndex(order["CurBarIndex"])
             return 1
         else:
             return 0
+
+    def _setConOpenTime(self, order, lastOrder):
+        """更新连续同向开仓次数"""
+        if self._maxConOpenTime == -1:
+            return
+
+        if len(lastOrder) < 1:
+            if order["Offset"] == oOpen or order["Offset"] == oNone:
+                self._conOpenTime += 1
+                return
+
+        if order["Offset"] == oOpen or order["Offset"] == oNone:   # 开仓单或外盘订单
+            if lastOrder["Direct"] == order["Direct"] and \
+                    lastOrder["Offset"] == order["Offset"]:  # 订单方向相同
+                self._conOpenTime += 1
+
+            else:
+                self._conOpenTime = 1
+
+        else:
+            self._conOpenTime = 0
+
+    def _setCurBarOpenTime(self, order):
+        """更新每根K线同向开仓次数"""
+        if self._maxCurBarOpenTime == -1:  # 每根K线同向开仓次数不做限制
+            return
+
+        if order["CurBarIndex"] == self._curBarIndex:
+
+            if order["Offset"] == oOpen or order["Offset"] == oNone:
+                self._setDCurBarOpenTime(order)
+
+        else:
+            if order["Offset"] == oOpen or order["Offset"] == oNone:
+                if order["Direct"] == dBuy and order["Offset"] == oOpen:  # 买开
+                    self._initCurBarOpenTime()
+                    self._curBarOpenTime["BuyOpen"] += 1
+
+                elif order["Direct"] == dSell and order["Offset"] == oOpen:  # 卖开
+                    self._initCurBarOpenTime()
+                    self._curBarOpenTime["SellOpen"] += 1
+
+                elif order["Direct"] == dBuy and order["Offset"] == oNone:  # 买
+                    self._initCurBarOpenTime()
+                    self._curBarOpenTime["Buy"] += 1
+
+                elif order["Direct"] == dSell and order["Offset"] == oNone:  # 卖
+                    self._initCurBarOpenTime()
+                    self._curBarOpenTime["Sell"] += 1
+                else:
+                    pass
+            else:
+                self._initCurBarOpenTime()
+
+    def _setHasOpen(self, order):
+        """更新当前K线是否有开仓单"""
+        # if order["Offset"] == oOpen:
+        #     self._hasOpen = True
+        if self._openNotReverse == 0:
+            return
+
+        if order["CurBarIndex"] == self._curBarIndex:
+            if order["Offset"] == oOpen:
+                self._hasOpen = True
+        else:
+            if order["Offset"] == oOpen:
+                self._hasOpen = True
+            else:
+                self._hasOpen = False
+
+    def _setHasCover(self, order):
+        """更新当前K线是否有平仓单"""
+        if self._coverNotOpen == 0:
+            return
+
+        if order["CurBarIndex"] == self._curBarIndex:
+            if order["Offset"] == oCover:
+                self._hasCover = True
+        else:
+            if order["Offset"] == oCover:
+                self._hasCover = True
+            else:
+                self._hasCover = False
+
 
 
 
