6fd0aa5dca4214739309747e81d012280ebc1e9d
gaoyantao134627 <603172532@qq.com>
2019-07-10 21:03:12 +0800

    版本提示

--- src/equant.py
+++ src/equant.py
@@ -21,7 +21,7 @@ def main():
     log_process = Process(target=run_log_process, args=(logger,))
     log_process.start()
     
-    logger.info("Start epolestar equant version:%s!"%("EquantV1.0.5.20190710"))
+    logger.info("Start epolestar equant version:%s!"%("EquantV1.0.6.20190710"))
 
     # 创建策略引擎到界面的队列，发送资金数据
     eg2ui_q = Queue(10000)
