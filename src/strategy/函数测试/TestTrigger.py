8a03c6894b138eb2549373415385818c51bc8060
lmx-hub <lmxeq5@qq.com>
2019-08-30 17:14:49 +0800

    --

--- src/strategy/函数测试/TestTrigger.py
+++ src/strategy/函数测试/TestTrigger.py
@@ -5,10 +5,11 @@ code2 = 'ZCE|F|TA|005'
 scode = 'SPD|s|TA|001|005'
 
 def initialize(context): 
-    SetBarInterval(scode, 'T', 0, 300)
-    SetBarInterval(code1, 'T', 0, 300)
-    SetBarInterval(code2, 'T', 0, 300)
+    SetBarInterval(scode, 'M', 1, 300)
+    SetBarInterval(code1, 'M', 1, 300)
+    SetBarInterval(code2, 'M', 1, 300)
 
 def handle_data(context):
-    LogInfo('quote time:', context.contractNo(), Q_UpdateTime())  
+    tim = context.dateTimeStamp()
+    LogInfo('quote time: %s.%s %s' % (tim[:8], tim[8:12], context.contractNo()))  
 
